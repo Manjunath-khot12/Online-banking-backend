1f7c95340c301db84c49d5de73fafdb8deec1f86
manjunath <manjukhot20148@gmail.com>
2024-09-04 23:05:12 +0530

    email sender

--- Online-Banking/src/main/java/com/excelR/banking/controller/RegistrationController.java
+++ Online-Banking/src/main/java/com/excelR/banking/controller/RegistrationController.java
@@ -1,11 +1,15 @@
 package com.excelR.banking.controller;
 
-import com.excelR.banking.model.User;
-import com.excelR.banking.serviceImpl.UserServiceImpl;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.excelR.banking.model.User;
+import com.excelR.banking.serviceImpl.EmailService;
+import com.excelR.banking.serviceImpl.UserServiceImpl;
 
 @RestController
 @RequestMapping("/banking")
@@ -13,6 +17,9 @@ public class RegistrationController {
 
     @Autowired
     private UserServiceImpl userServiceImpl;
+    
+    @Autowired
+    private EmailService emailService;
 
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody User user) {
@@ -21,6 +28,16 @@ public class RegistrationController {
                 return ResponseEntity.badRequest().body("Email cannot be null or empty.");
             }
             userServiceImpl.registerUser(user);
+            
+            String subject = "Registration Successful";
+            String body = "Dear " + user.getFirstName() + ",\n\n" +
+                          "Your registration is successful. Here are your login details:\n\n" +
+                          "Username: " + user.getEmail() + "\n" +
+                          "Password: " + user.getPassword() + "\n\n" +
+                          "Thank you for Registering with us.";
+            
+            emailService.sendRegistrationEmail(user.getEmail(), subject, body);
+            
             return ResponseEntity.ok("Registration successful");
         } catch (Exception e) {
             e.printStackTrace();
