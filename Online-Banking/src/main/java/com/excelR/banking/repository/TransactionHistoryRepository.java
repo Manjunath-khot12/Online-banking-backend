63713d9c84a6c90c9f67717a7291418be87b8619
manjunath <manjukhot20148@gmail.com>
2024-09-10 15:02:37 +0530

    getting transactional history commit

--- Online-Banking/src/main/java/com/excelR/banking/repository/TransactionHistoryRepository.java
+++ Online-Banking/src/main/java/com/excelR/banking/repository/TransactionHistoryRepository.java
@@ -1,8 +1,14 @@
 package com.excelR.banking.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+
 import com.excelR.banking.model.TransactionHistory;
 
 public interface TransactionHistoryRepository extends JpaRepository<TransactionHistory, Long> {
-    
+	@Query("SELECT t FROM TransactionHistory t WHERE t.sourceAccount = :accountNumber OR t.destinationAccount = :accountNumber")
+    List<TransactionHistory> findBySourceOrDestinationAccount( Long accountNumber);
 }
